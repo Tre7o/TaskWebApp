a5776e556c6a616f2dc74ef1cf1fc1ed98d37753
John Trevor <suubitrevor143@gmail.com>
2024-06-21 13:03:08 +0300

    Add project files.

---
+++ TaskWebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            ViewBag.Message = "This is a description of this page";
+
+            return View();
+        }
+    }
+}
---
+++ TaskWebApp/Services/Data/TaskRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskWebApp.Models;
+
+namespace TaskWebApp.Services.Data
+{
+    public class TaskRepo
+    {
+        internal bool SaveTask(Task task)
+        {
+            return true;
+        }
+    }
+}
